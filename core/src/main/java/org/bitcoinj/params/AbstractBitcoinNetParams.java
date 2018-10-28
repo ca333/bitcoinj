c8c51b0068832216ad5470bd08a50446326dbbbd
ca333 <ca333@keemail.me>
2018-10-28 09:28:01 +0100

    add kmd netparams class

--- core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
+++ core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
@@ -36,132 +36,138 @@ import java.util.concurrent.TimeUnit;
 /**
  * Parameters for Bitcoin-like networks.
  */
-public abstract class AbstractBitcoinNetParams extends NetworkParameters {
-    /**
-     * Scheme part for Bitcoin URIs.
-     */
-    public static final String BITCOIN_SCHEME = "bitcoincash";
+ public abstract class AbstractKomodoMainNetParams extends NetworkParameters {
 
-    private static final Logger log = LoggerFactory.getLogger(AbstractBitcoinNetParams.class);
+     /**
+      * Scheme part for Bitcoin URIs.
+      */
+     public static final String BITCOIN_SCHEME = "komodo";
+     public static final int REWARD_HALVING_INTERVAL = 840000;
 
-    public AbstractBitcoinNetParams() {
-        super();
-    }
+     private static final Logger log = LoggerFactory.getLogger(AbstractKomodoMainNetParams.class);
 
-    /**
-     * Checks if we are at a difficulty transition point.
-     * @param storedPrev The previous stored block
-     * @param parameters The network parameters
-     * @return If this is a difficulty transition point
-     */
-    public static boolean isDifficultyTransitionPoint(StoredBlock storedPrev, NetworkParameters parameters) {
-        return ((storedPrev.getHeight() + 1) % parameters.getInterval()) == 0;
-    }
+     public AbstractKomodoMainNetParams() {
+         super();
+     }
 
-    /**
-     * determines whether monolith upgrade is activated based on MTP
-     * @param storedPrev The previous stored block
-     * @param store BlockStore containing at least 11 blocks
-     * @param parameters The network parameters
-     * @return
-     */
-    public static boolean isMonolithEnabled(StoredBlock storedPrev, BlockStore store, NetworkParameters parameters) {
-        if (storedPrev.getHeight() < 524626) { //current height at time of writing, well below the activation block height
-            return false;
-        }
-        try {
-            long mtp = BlockChain.getMedianTimestampOfRecentBlocks(storedPrev, store);
-            return isMonolithEnabled(mtp, parameters);
-        } catch (BlockStoreException e) {
-            throw new RuntimeException("Cannot determine monolith activation without BlockStore");
-        }
-    }
+     /**
+      * Checks if we are at a reward halving point.
+      * @param height The height of the previous stored block
+      * @return If this is a reward halving point
+      */
+     public final boolean isRewardHalvingPoint(final int height) {
+         return ((height + 1) % REWARD_HALVING_INTERVAL) == 0;
+     }
 
-    /**
-     * determines whether monolith upgrade is activated based on the given MTP.  Useful for overriding MTP for testing.
-     * @param medianTimePast
-     * @param parameters The network parameters
-     * @return
-     */
-    public static boolean isMonolithEnabled(long medianTimePast, NetworkParameters parameters) {
-        return medianTimePast >= parameters.getMonolithActivationTime();
-    }
+     /**
+      * Checks if we are at a difficulty transition point.
+      * @param height The height of the previous stored block
+      * @return If this is a difficulty transition point
+      */
+     public final boolean isDifficultyTransitionPoint(final int height) {
+         return ((height + 1) % this.getInterval()) == 0;
+     }
 
-    /**
-     * The number that is one greater than the largest representable SHA-256
-     * hash.
-     */
-    private static BigInteger LARGEST_HASH = BigInteger.ONE.shiftLeft(256);
+     @Override
+     public void checkDifficultyTransitions(final StoredBlock storedPrev, final Block nextBlock,
+                                            final BlockStore blockStore) throws VerificationException, BlockStoreException {
+         final Block prev = storedPrev.getHeader();
 
-    /**
-     * Compute the a target based on the work done between 2 blocks and the time
-     * required to produce that work.
-     */
-     public static BigInteger ComputeTarget(StoredBlock pindexFirst,
-                                   StoredBlock pindexLast) {
+         // Is this supposed to be a difficulty transition point?
+         if (!isDifficultyTransitionPoint(storedPrev.getHeight())) {
 
-         Preconditions.checkState(pindexLast.getHeight() > pindexFirst.getHeight());
+             // No ... so check the difficulty didn't actually change.
+             if (nextBlock.getDifficultyTarget() != prev.getDifficultyTarget())
+                 throw new VerificationException("Unexpected change in difficulty at height " + storedPrev.getHeight() +
+                         ": " + Long.toHexString(nextBlock.getDifficultyTarget()) + " vs " +
+                         Long.toHexString(prev.getDifficultyTarget()));
+             return;
+         }
 
-        /*
-         * From the total work done and the time it took to produce that much work,
-         * we can deduce how much work we expect to be produced in the targeted time
-         * between blocks.
-         */
-        BigInteger work = pindexLast.getChainWork().subtract(pindexFirst.getChainWork());
-        work = work.multiply(BigInteger.valueOf(TARGET_SPACING));
+         // We need to find a block far back in the chain. It's OK that this is expensive because it only occurs every
+         // two weeks after the initial block chain download.
+         final Stopwatch watch = Stopwatch.createStarted();
+         Sha256Hash hash = prev.getHash();
+         StoredBlock cursor = null;
+         final int interval = this.getInterval();
+         for (int i = 0; i < interval; i++) {
+             cursor = blockStore.get(hash);
+             if (cursor == null) {
+                 // This should never happen. If it does, it means we are following an incorrect or busted chain.
+                 throw new VerificationException(
+                         "Difficulty transition point but we did not find a way back to the last transition point. Not found: " + hash);
+             }
+             hash = cursor.getHeader().getPrevBlockHash();
+         }
+         checkState(cursor != null && isDifficultyTransitionPoint(cursor.getHeight() - 1),
+                 "Didn't arrive at a transition point.");
+         watch.stop();
+         if (watch.elapsed(TimeUnit.MILLISECONDS) > 50)
+             log.info("Difficulty transition traversal took {}", watch);
 
-        // In order to avoid difficulty cliffs, we bound the amplitude of the
-        // adjustement we are going to do.
-        //assert(pindexLast->nTime > pindexFirst->nTime);
-        long nActualTimespan = pindexLast.getHeader().getTimeSeconds() - pindexFirst.getHeader().getTimeSeconds();
-        if (nActualTimespan > 288 * TARGET_SPACING) {
-            nActualTimespan = 288 * TARGET_SPACING;
-        } else if (nActualTimespan < 72 * TARGET_SPACING) {
-            nActualTimespan = 72 * TARGET_SPACING;
-        }
+         Block blockIntervalAgo = cursor.getHeader();
+         int timespan = (int) (prev.getTimeSeconds() - blockIntervalAgo.getTimeSeconds());
+         // Limit the adjustment step.
+         final int targetTimespan = this.getTargetTimespan();
+         if (timespan < targetTimespan / 4)
+             timespan = targetTimespan / 4;
+         if (timespan > targetTimespan * 4)
+             timespan = targetTimespan * 4;
 
-        work = work.divide(BigInteger.valueOf(nActualTimespan));
+         BigInteger newTarget = Utils.decodeCompactBits(prev.getDifficultyTarget());
+         newTarget = newTarget.multiply(BigInteger.valueOf(timespan));
+         newTarget = newTarget.divide(BigInteger.valueOf(targetTimespan));
 
-        /*
-         * We need to compute T = (2^256 / W) - 1 but 2^256 doesn't fit in 256 bits.
-         * By expressing 1 as W / W, we get (2^256 - W) / W, and we can compute
-         * 2^256 - W as the complement of W.
-         */
-         return LARGEST_HASH.divide(work).subtract(BigInteger.ONE);//target.add(BigInteger.ONE))
-    }
+         if (newTarget.compareTo(this.getMaxTarget()) > 0) {
+             log.info("Difficulty hit proof of work limit: {}", newTarget.toString(16));
+             newTarget = this.getMaxTarget();
+         }
 
-    @Override
-    public Coin getMaxMoney() {
-        return MAX_MONEY;
-    }
+         int accuracyBytes = (int) (nextBlock.getDifficultyTarget() >>> 24) - 3;
+         long receivedTargetCompact = nextBlock.getDifficultyTarget();
 
-    @Override
-    public Coin getMinNonDustOutput() {
-        return Transaction.MIN_NONDUST_OUTPUT;
-    }
+         // The calculated difficulty is to a higher precision than received, so reduce here.
+         BigInteger mask = BigInteger.valueOf(0xFFFFFFL).shiftLeft(accuracyBytes * 8);
+         newTarget = newTarget.and(mask);
+         long newTargetCompact = Utils.encodeCompactBits(newTarget);
 
-    @Override
-    public MonetaryFormat getMonetaryFormat() {
-        return new MonetaryFormat();
-    }
+         if (newTargetCompact != receivedTargetCompact)
+             throw new VerificationException("Network provided difficulty bits do not match what was calculated: " +
+                     Long.toHexString(newTargetCompact) + " vs " + Long.toHexString(receivedTargetCompact));
+     }
 
-    @Override
-    public int getProtocolVersionNum(final ProtocolVersion version) {
-        return version.getBitcoinProtocolVersion();
-    }
+     @Override
+     public Coin getMaxMoney() {
+         return MAX_MONEY;
+     }
 
-    @Override
-    public BitcoinSerializer getSerializer(boolean parseRetain) {
-        return new BitcoinSerializer(this, parseRetain);
-    }
+     @Override
+     public Coin getMinNonDustOutput() {
+         return Transaction.MIN_NONDUST_OUTPUT;
+     }
 
-    @Override
-    public String getUriScheme() {
-        return BITCOIN_SCHEME;
-    }
+     @Override
+     public MonetaryFormat getMonetaryFormat() {
+         return new MonetaryFormat();
+     }
 
-    @Override
-    public boolean hasMaxMoney() {
-        return true;
-    }
-}
+     @Override
+     public int getProtocolVersionNum(final ProtocolVersion version) {
+         return version.getBitcoinProtocolVersion();
+     }
+
+     @Override
+     public BitcoinSerializer getSerializer(boolean parseRetain) {
+         return new BitcoinSerializer(this, parseRetain);
+     }
+
+     @Override
+     public String getUriScheme() {
+         return BITCOIN_SCHEME;
+     }
+
+     @Override
+     public boolean hasMaxMoney() {
+         return true;
+     }
+ }
